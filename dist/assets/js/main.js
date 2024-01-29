ce4103b5c4a7261c6e75264ea2ed442053c6ace4
Matheus <matheusteodorogarcia@gmail.com>
2024-01-29 12:45:26 -0300

    Imagens, html e estilos iniciais

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,2 +1,12 @@
-(()=>{"use strict";console.log("Hello World!")})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!***********************!*\
+  !*** ./src/script.ts ***!
+  \***********************/
+
+
+console.log('Hello World!');
+/******/ })()
+;
 //# sourceMappingURL=main.js.map
